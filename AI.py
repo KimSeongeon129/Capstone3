9a783607e6189c28773e13eb8737a9e85d51ffa0
HSM0000 <min06890@naver.com>
2022-10-31 20:36:46 +0900

    수정

--- AI.py
+++ AI.py
@@ -1,5 +1,22 @@
-import os
+from flask import Flask, render_template
+from flask import jsonify,url_for,redirect,request,Blueprint,g
+import requests
+import json
+from flask import g
+#이미지 불량의 따른 이미지 종류 반환
 
-my_img = '경로'
-terminal_command = f"python3 model/detect.py --weights model/yolov7.pt --conf 0.25 --img-size 640 --source {my_img}"
-os.system(terminal_command)
+def check_type(label):
+    type=""
+    if label=='Overlap'or label=='Undercut':
+        type='버트조인트'
+    elif label=='Bad binding'or label=='Poor installation' or label=='Cable damage':
+        type='케이블'
+    elif label=='Duct damage'or label=='Bad connection' or label=='Bad tape':
+        type='덕트'
+    elif label in ['Poor bolting', 'Pipe damage']:
+        type='선박 배관'
+    elif label in ['Step difference', 'Poor painting', 'Poor installation of reinforcement']:
+        type='선체'
+    else :
+        type='다른거'       
+    return type
