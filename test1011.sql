ac53e5f69f6a4b86faa6781ed2ccc608f2a16d4f
minsungeee <a41937164@gmail.com>
2022-10-31 22:18:59 +0900

    gg

--- test1011.sql
+++ test1011.sql
@@ -8,7 +8,7 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 CREATE DATABASE IF not exists myDB;
 
 
-CREATE TABLE IF NOT EXISTS `user` (
+CREATE TABLE IF NOT EXISTS myDB.`user` (
   `user_id` VARCHAR(100) NOT NULL,
   `nickname` VARCHAR(100) NOT NULL,
   `user_admin` VARCHAR(100) ,
@@ -17,7 +17,7 @@ CREATE TABLE IF NOT EXISTS `user` (
 ENGINE = InnoDB;
 
 
-CREATE TABLE IF NOT EXISTS `admin` (
+CREATE TABLE IF NOT EXISTS myDB.`admin` (
   `admin_id` VARCHAR(100) NOT NULL,
   `admin_pw` VARCHAR(100) NOT NULL,
   `number` VARCHAR(100) NOT NULL,
@@ -27,36 +27,30 @@ ENGINE = InnoDB;
 
 
 
-CREATE TABLE IF NOT EXISTS `image` (
+CREATE TABLE IF NOT EXISTS myDB.`image` (
   `inspection_number` VARCHAR(50) NOT NULL,
-  `img_id` VARCHAR(45) NOT NULL,
   `bbox_x1` DOUBLE NOT NULL,
   `bbox_x2` DOUBLE NOT NULL,
   `bbox_y1` DOUBLE NOT NULL,
   `bbox_y2` DOUBLE NOT NULL,
-  `image` MEDIUMBLOB NOT NULL,
+  `image`  VARCHAR(100),
   PRIMARY KEY (`inspection_number`))
 ENGINE = InnoDB;
 
 
 
-CREATE TABLE IF NOT EXISTS `result` (
+CREATE TABLE IF NOT EXISTS myDB.`result` (
   `part_id` VARCHAR(45) NOT NULL,
-  `date` DATETIME(6) NOT NULL,
   `part_name` VARCHAR(45) NOT NULL,
   `part_category` VARCHAR(45) NOT NULL,
   `part_judge` VARCHAR(45) NOT NULL,
   `user_id` VARCHAR(100) NOT NULL,
-  `inspection_number` VARCHAR(50) NOT NULL,
-  PRIMARY KEY (`inspection_number`),
+  `inspection_number` VARCHAR(100) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+   `date` timestamp default now(),
   FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE cascade ON UPDATE cascade,
   FOREIGN KEY (`inspection_number`) REFERENCES `image` (`inspection_number`) ON DELETE cascade ON UPDATE cascade)
 ENGINE = InnoDB;
 
-select * from admin;
-
-
-select * from user;
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
