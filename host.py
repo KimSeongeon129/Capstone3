9435ed78defa7a4f26ac4522dfcd09f401fc604f
HSM0000 <min06890@naver.com>
2022-11-01 21:57:55 +0900

    수정

---
+++ host.py
@@ -0,0 +1,3 @@
+def ip_check():
+    ip="http://127.0.0.1:5000"
+    return ip
--- login.py
+++ login.py
@@ -3,20 +3,26 @@ from flask import jsonify,url_for,redirect,request,Blueprint,g
 from db1011 import find_id_user,find_id_admin, add_user
 import requests
 import json
+from host import ip_check
 from flask import g
 
 bp= Blueprint('login',__name__)
 @bp.route("/")
 def login():
     return render_template("login.html")
-
+ip_url=ip_check()
 #카카오 로그인
+@bp.route("/kakao")
+def kakao():
+    url="https://kauth.kakao.com/oauth/authorize?client_id=32e62fdd5c6f676f20e8792d524c06b9&redirect_uri="+ip_url+"/oauth&response_type=code"
+    print(url)
+    return redirect(url)
 @bp.route("/oauth")
 def oauth_api():
     #카카오 코드 가져오기
     code=str(request.args.get('code'))
     url = "https://kauth.kakao.com/oauth/token"
-    payload="grant_type=authorization_code&client_id=32e62fdd5c6f676f20e8792d524c06b9&redirect_uri=http://127.0.0.1:5000%2Foauth&code="+str(code)
+    payload="grant_type=authorization_code&client_id=32e62fdd5c6f676f20e8792d524c06b9&redirect_uri="+ip_url+"%2Foauth&code="+str(code)
     headers= {
         'Content-Type': "application/x-www-form-urlencoded",
         'Cache-control':"no-cache",
@@ -57,7 +63,7 @@ def oauth_api():
 #네이버 로그인
 @bp.route("/naver")
 def naver():
-    url="https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=CZ4CAklls0R3pDVGPNhs&state=32raedfa38usf&redirect_uri=http://localhost:5000/naver_login"
+    url="https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=CZ4CAklls0R3pDVGPNhs&state=32raedfa38usf&redirect_uri="+ip_url+"/naver_login"
     return redirect(url)
 @bp.route("/naver_login")
 def naver_login():
@@ -82,8 +88,8 @@ def naver_login():
     find_id= find_id_user(g.db,id) #디비에서 id 가져오기
     # db에 아이디가 존재하면
     if (find_id) :
-        return redirect("/user_main")
         session['username']=id
+        return redirect("/user_main")
     else : #db에 아이디가 존재 하지 않는 경우
         #db에 저장
         add_user(g.db, id, name)
