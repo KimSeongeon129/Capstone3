d2c5951a6b93796a88a270e9c9383546a643a696
HSM0000 <min06890@naver.com>
2022-11-08 00:25:57 +0900

    수정

--- list.py
+++ list.py
@@ -16,17 +16,20 @@ def list():
     else: #필터링 한거 보여주기
         #필터링 내용 가져오기
         filtering='part_judge'
-        data='불량품'
+        data='불량'
         id=session['username']
         #필터링 내용으로 db에서 데이터 가져오기
         list = user_result(g.db,id)
         list = sorted(list, key= lambda x: x['date'], reverse=True)#최신순으로 반환
         dic=[]
+        c=0
         for i in list:#예시로 불량품만 뽑기
             if i[filtering]==data:
                 dic.append(i)
+                c=c+1
+        if c==0:
+            dic=['no']
         print(dic)
-        #데이터 가져온거 반환하기
-        return id
+        return render_template("list.html", list = dic)
 
 
