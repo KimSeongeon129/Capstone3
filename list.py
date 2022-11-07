5d45cf6b0a4132f3e9bc4a43c8f136ac10b1c14c
HSM0000 <min06890@naver.com>
2022-11-07 23:00:36 +0900

    수정

--- list.py
+++ list.py
@@ -5,10 +5,17 @@ import requests
 import json
 
 bp= Blueprint('list',__name__)
-@bp.route('/list')#내역조회페이지
+@bp.route('/list',methods=['GET','POST'])#내역조회페이지
 def list():    
-    id=session['username']
-    list = user_result(g.db,id)
-    print(list)
-    return render_template("list.html", list = list)
+    if request.method=='GET':#그냥 내역조회 했을시 전체 보여주기
+        id=session['username']
+        list = user_result(g.db,id)
+        print(list)
+        return render_template("list.html", list = list)
+    else: #필터링 한거 보여주기
+        #필터링 내용 가져오기
+        #필터링 내용으로 db에서 데이터 가져오기
+        #데이터 가져온거 반환하기
+        return id
+
 
