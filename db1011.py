1d3ee6801f4561d6d90b9c0c0933875eedb1607f
minsungeee <115096802+minsungeee@users.noreply.github.com>
2022-11-13 21:16:54 +0900

    defective_rate

--- db1011.py
+++ db1011.py
@@ -3,7 +3,7 @@ from contextlib import nullcontext
 from pymysql import cursors, connect
 import pymysql
 import bcrypt
-
+import pandas as pd
 def init_db():
 
     db = connect(host='127.0.0.1', user='root', password='root', db='mydb', charset='utf8',cursorclass=pymysql.cursors.DictCursor)
@@ -20,7 +20,7 @@ def init_db():
     
         sql1= "CREATE TABLE IF NOT EXISTS mydb.`user` (`user_id` VARCHAR(100) NOT NULL, `nickname` VARCHAR(100) NOT NULL,`user_admin` VARCHAR(100) , `user_line` VARCHAR(100) ,PRIMARY KEY (`user_id`))"
         sql2= "CREATE TABLE IF NOT EXISTS mydb.`admin` (`admin_id` VARCHAR(100) NOT NULL, `admin_pw` VARCHAR(100) NOT NULL, `number` VARCHAR(100) NOT NULL, `name` VARCHAR(100) NOT NULL,PRIMARY KEY (`admin_id`))"
-        sql3= "CREATE TABLE IF NOT EXISTS mydb.`result` (`part_id` VARCHAR(45) NOT NULL, `part_name` VARCHAR(45) NOT NULL, `part_category` VARCHAR(45) NOT NULL, `part_judge` VARCHAR(45) NOT NULL, `user_id` VARCHAR(100) NOT NULL, `inspection_number` INT(100) NOT NULL AUTO_INCREMENT PRIMARY KEY, `date` TIMESTAMP DEFAULT NOW()) "
+        sql3= "CREATE TABLE IF NOT EXISTS mydb.`result` (`part_id` VARCHAR(45) NOT NULL, `part_name` VARCHAR(45) NOT NULL, `part_category` VARCHAR(45) NOT NULL, `part_judge` VARCHAR(45) NOT NULL, `user_id` VARCHAR(100) NOT NULL, `inspection_number` INT(100) NOT NULL AUTO_INCREMENT PRIMARY KEY, `date` TIMESTAMP DEFAULT NOW(), `defective_rate` float NULL) "
         sql4= "CREATE TABLE IF NOT EXISTS mydb.`image` (`inspection_number` INT(100) NOT NULL,`bbox_x1` DOUBLE NOT NULL,`bbox_x2` DOUBLE NOT NULL,`bbox_y1` DOUBLE NOT NULL,`bbox_y2` DOUBLE NOT NULL,`image` VARCHAR(100) NOT NULL, PRIMARY KEY (`inspection_number`), FOREIGN KEY (`inspection_number`) REFERENCES `result` (`inspection_number`) ON DELETE cascade ON UPDATE cascade) "
         
         cursor.execute(sql1)   
@@ -246,6 +246,14 @@ def update_result(db, part_category, part_judge,  inspection_number):
         db.commit()
         
         
+def get_statistics(db):
+    with db.cursor() as cursor:
+        sql= "select * from result"
+        df=pd.read_sql(sql,db)
+        print(df)
+        print(type(df))
+        # result = cursor.fetchone()
+
 
 
 #db.close()
