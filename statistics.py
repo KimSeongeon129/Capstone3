cea30b0f2c2ed80d74af2fc192e3c5099ac005e5
KimSeongeon129 <seongeon129@gmail.com>
2022-11-18 20:38:00 +0900

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -8,7 +8,7 @@ import numpy as np
 import math
 
 from bokeh.plotting import figure, output_file, show
-from bokeh.models import ColumnDataSource, MultiChoice, CustomJS
+from bokeh.models import ColumnDataSource, MultiChoice, CustomJS, value
 from bokeh.layouts import row, column, gridplot, layout
 from bokeh.embed import components
 from bokeh.io import show
@@ -39,11 +39,11 @@ def imgUpload_result():
                         counts[PARTS.index(part)] = valueCounts[part]
                 except:
                         pass
-
+        
         source = ColumnDataSource(data=dict(parts=PARTS, counts=counts))
         p1_cmap = factor_cmap('parts', palette=PuBuGn6, factors=PARTS)
         
-        p1 = figure(x_range=PARTS, height=600, toolbar_location=None, title="부품별 불량품 건수")
+        p1 = figure(x_range=PARTS, width=500, height=600, toolbar_location=None, title="부품별 불량품 건수")
         p1.vbar(x='parts', top='counts', width=0.9, source=source,
                 line_color='white', fill_color=p1_cmap)
 
@@ -69,16 +69,20 @@ def imgUpload_result():
         defect_cable = [defect for defect in cable_def]
         defect_lagging = [defect for defect in lagging_def]
         part_category = defect_duct + defect_pipe + defect_hull + defect_cable + defect_lagging
+
+        count = [0 for i in part_category]
+        for p in df[df['part_name'] != '양품']['part_category'].value_counts().index:
+                count[part_category.index(p)] = df[df['part_name'] != '양품']['part_category'].value_counts()[p]
         
         df1 = pd.DataFrame({'part_name' : part_name,
                    'part_category' : part_category,
-                   'count' : [1 for i in part_category]})
+                   'count' : count})
         group = df1.groupby(['part_name', 'part_category'])
         
         p2_cmap = factor_cmap('part_name_part_category', palette=PuBuGn6, factors=PARTS, end=1)
         
-        p2 = figure(width=500, height=600, title="불량 유형별 불량품 건수",
-                x_range=group, toolbar_location=None, 
+        p2 = figure(width=700, height=600, title="불량 유형별 불량품 건수",
+                x_range=group, toolbar_location=None,  margin = (0, 0, 0, 70),
                 tooltips=[("count", "@count_count"), ("part_name, part_category", "@part_name_part_category")])
         p2.vbar(x='part_name_part_category', top='count_count', width=1, source=group,
                 line_color="white", fill_color = p2_cmap)
@@ -88,15 +92,122 @@ def imgUpload_result():
         p2.xgrid.grid_line_color = None
         p2.xaxis.major_label_orientation = 1.2
         p2.outline_line_color = None
+        
+        #-------------------------------
+        #  텍스트 시각화
+        #-------------------------------
 
+        p3 = figure(width=170, height=200,
+                active_drag = None,
+                toolbar_location=None,
+                margin = (0, 0, 0, 70))
+        p3.xgrid.visible = False
+        p3.ygrid.visible = False
+        p3.xaxis.visible = False
+        p3.yaxis.visible = False
+        p3.outline_line_color = 'white'
+        p3.title = "총 검사 수"
+        p3.title.align = 'center'
+        
+        p3.text(
+                x="x",
+                y="y",
+                text="text",
+                text_align="center",
+                text_font_size="30px",
+                text_font=value("Verdana"),
+                text_font_style="normal",
+                source=ColumnDataSource(
+                pd.DataFrame.from_records(
+                        [
+                        dict(
+                                x=100,
+                                y=50,
+                                text=f"{len(df)}",
+                                color="black",
+                        )
+                        ]
+                )
+                ),
+        )
+        
+        p4 = figure(width=170, height=200,
+                active_drag = None,
+                toolbar_location=None)
+        p4.xgrid.visible = False
+        p4.ygrid.visible = False
+        p4.xaxis.visible = False
+        p4.yaxis.visible = False
+        p4.outline_line_color = 'white'
+        p4.title = "총 불량품 수"
+        p4.title.align = 'center'
+
+        p4.text(
+                x="x",
+                y="y",
+                text="text",
+                text_align="center",
+                text_font_size="30px",
+                text_font=value("Verdana"),
+                text_font_style="normal",
+                source=ColumnDataSource(
+                pd.DataFrame.from_records(
+                        [
+                        dict(
+                                x=100,
+                                y=50,
+                                text=f"{len(df[df['part_name'] != '양품'])}",
+                                color="black",
+                        )
+                        ]
+                )
+                ),
+        )
+        
+        p5 = figure(width=170, height=200,
+                active_drag = None,
+                toolbar_location=None)
+        p5.xgrid.visible = False
+        p5.ygrid.visible = False
+        p5.xaxis.visible = False
+        p5.yaxis.visible = False
+        p5.outline_line_color = 'white'
+        p5.title = "불량품 수"
+        p5.title.align = 'center'
+        
+        p5.text(
+                x="x",
+                y="y",
+                text="text",
+                text_align="center",
+                text_font_size="30px",
+                text_font=value("Verdana"),
+                text_font_style="normal",
+                source=ColumnDataSource(
+                pd.DataFrame.from_records(
+                        [
+                        dict(
+                                x=100,
+                                y=50,
+                                text=f"{len(df[df['part_name'] != '양품'])}",
+                                color="black",
+                        )
+                        ]
+                )
+                ),
+        )
 
         #-------------------------------
         #  Laying out in row
         #-------------------------------
 
-        multi_choice = MultiChoice(value=PARTS, options=PARTS, title='parts:')
+        multi_choice = MultiChoice(value=PARTS, options=PARTS, title='parts:',width=600, height=200)
 
-        callback = CustomJS(args=dict(source=source, p1 = p1, p2 = p2, p2_index = group.describe().index, multi_choice=multi_choice), code="""    
+        callback = CustomJS(args=dict(source=source,
+                                      p1 = p1, 
+                                      p2 = p2, p2_index = group.describe().index, 
+                                      p5 = p5,
+                                      multi_choice=multi_choice), code="""    
                 var selected = multi_choice.value;
                 var p2_index_list = [];
                 p1.x_range.factors = selected;
@@ -107,14 +218,14 @@ def imgUpload_result():
                                         p2_index_list.push(p2_index[j]);
                                 }
                         }
-                }    
+                }
                 p2.x_range.factors = p2_index_list;
-
+                console.log(p5.text)
         """)
         multi_choice.js_on_change('value', callback)
 
-        plot = layout(children=[multi_choice,
-                                [p1, p2]], sizing_mode='stretch_both')
+        plot = layout(children=[[multi_choice,p3, p4, p5],
+                                [p1, p2]], sizing_mode='fixed')
 
         script, div = components(plot)
 
